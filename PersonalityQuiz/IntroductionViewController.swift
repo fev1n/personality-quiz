13d8289010135bd94072925330b5b8de12ea6bf0
Fevin Patel <132969492+fev1n@users.noreply.github.com>
2023-11-23 23:03:33 -0500

    Add files via upload

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Fevin Patel on 2023-11-18.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Fevin Patel on 2023-11-19.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [:]) {
+            (counts, answer) in
+            counts[answer.type, default: 0] += 1
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by: {
+            (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
